168a6f34e812229625d97cac3574e27ddf55a530
joicianogomesosares <joirodrigues822@gmail.com>
2026-05-29 01:19:00 -0300

    Update teste.js

--- netlify/functions/teste.js
+++ netlify/functions/teste.js
@@ -1,12 +1,16 @@
 exports.handler = async (event) => {
+  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+  const SUPABASE_URL = process.env.SUPABASE_URL;
+  const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
     body: JSON.stringify({
       ok: true,
-      method: event.httpMethod,
-      has_api_key: !!process.env.ANTHROPIC_API_KEY,
-      api_key_prefix: process.env.ANTHROPIC_API_KEY ? process.env.ANTHROPIC_API_KEY.substring(0, 15) : 'NOT SET'
+      google_api_key: GOOGLE_API_KEY ? GOOGLE_API_KEY.substring(0, 8) + '...' : 'NAO DEFINIDA',
+      supabase_url: SUPABASE_URL ? 'OK' : 'NAO DEFINIDA',
+      supabase_key: SUPABASE_KEY ? 'OK' : 'NAO DEFINIDA'
     })
   };
 };
